a4ff611dbac0c59c0749e588e8596900a80cba11
devapromix <maxwof@ukr.net>
2025-06-14 16:05:14 +0300

    Added combat.lua

--- commands/kill.lua
+++ commands/kill.lua
@@ -5,10 +5,12 @@ function kill.exec(command_parts, player, map_data, items_data, enemies_data, sk
         output.add("Please specify an enemy to kill (e.g., 'kill Goblin').\n")
         return player
     end
+    
     local enemy_name = commands.get_item_name_from_parts(command_parts, 2)
     if commands.validate_parameter(enemy_name, "enemy", output) then
-        player_module.attack_enemy(enemy_name, map_data, player, enemies_data, items_data, skills_data, time, map, output)
+        combat.attack_enemy(enemy_name, map_data, player, enemies_data, items_data, skills_data, time, map, output, player_module)
     end
+    
     return player
 end
 
