317d021df394b0b5d6370e252995e4e64532fef8
devapromix <maxwof@ukr.net>
2025-06-26 10:30:21 +0300

    Added 'gold' command

--- commands.lua
+++ commands.lua
@@ -7,7 +7,7 @@ local command_map = {
 	game = {"help", "intro", "new", "load", "save", "about", "quit"},
 	info = {"status", "skills", "time", "items", "map", "gear"},
 	item = {"eat", "drink", "pick", "drop", "equip", "unequip"},
-	action = {"rest", "examine", "look", "kill", "light", "volume", "recipes", "cook", "fish", "trollcave", "village", "train", "enter", "leave", "buy", "sell"},
+	action = {"rest", "examine", "look", "kill", "light", "volume", "recipes", "cook", "fish", "trollcave", "village", "train", "enter", "leave", "buy", "sell", "gold"},
 	movement = {"north", "south", "east", "west", "n", "s", "e", "w", "up", "down", "u", "d"}
 }
 
@@ -230,6 +230,8 @@ function commands.handle_action_commands(cmd, command_parts, player, map_data, i
 		return command_leave.exec(player, map_data)
 	elseif cmd == "buy" then
 		return command_buy.exec(command_parts, player, game_time, time, player_module)
+	elseif cmd == "gold" then
+		return command_gold.exec(command_parts, player, player_module)
 	end
 	return player
 end
