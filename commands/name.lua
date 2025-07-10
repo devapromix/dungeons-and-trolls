7d6d99300442e6a3bb56fad22546b4ea4045de0b
devapromix <maxwof@ukr.net>
2025-07-10 19:32:38 +0300

    Improved name command.

--- commands/name.lua
+++ commands/name.lua
@@ -10,6 +10,14 @@ function name.exec(command_parts, player)
 	end
 	local new_name = table.concat(command_parts, " ", 2)
 	if new_name:match("^[a-zA-Z]+$") then
+		if string.len(new_name) < 3 then
+			output.add("Name must be at least 3 characters long.\n")
+			return player
+		end
+		if player.name ~= "Player" then
+			output.add("You can only set your hero's name once.\n")
+			return player
+		end
 		new_name = new_name:sub(1, 1):upper() .. new_name:sub(2)
 		player.name = new_name
 		output.add("Your name has been changed to: " .. new_name .. "\n")
